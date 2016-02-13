3c281516d8fd561480864cc840df30193b101304
David Huang <david.hhs@gmail.com>
2016-02-13 20:41:16 +0800

    assignment done

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+#The structure of my function is similar to the examples given by the instructors. mean() is replaced by 'solve' and vector is replaced by matrix()
+#The rest of the functions are the same.
 
 ## Write a short comment describing this function
+#inverse_m stands for inverse of a matrix; replaces 'm'
+#solve(x) is used to replace mean(x)
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        inverse_m <- NULL
+        set <- function(y) {
+                x <<- y
+                inverse_m <<- NULL
+        }
+        get <- function() x
+        seti <- function(inverse_input) inverse_m <<- inverse_input
+        geti <- function() inverse_m
+        list(set = set, get = get,
+             seti = seti,
+             geti = geti)
+        
 }
 
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
+cacheSolve <- function(x) {
         ## Return a matrix that is the inverse of 'x'
+        inverse_m <- x$geti()
+        if(!is.null(inverse_m)) {
+                message("getting cached data")
+                return(inverse_m)
+        }
+        data <- x$get()
+        inverse_m <- solve(data)
+        x$setmean(inverse_m)
+        inverse_m
 }
